c1c3bc9585e59f065b9be73b29b58217ff300ecc
Oluwatimilehin Dada <timilehinfavour14@gmail.com>
2023-02-05 22:05:13 +0100

    day5 update

--- script.js
+++ script.js
@@ -7,25 +7,33 @@ var error = document.getElementsByClassName("error");
 var tag = document.getElementsByTagName("a");
 var Input = document.getElementsByTagName("input")
 var nameCard = document.getElementById("card-name")
+var label = document.getElementsByTagName("label")
 
-//Function to display error message
-submit.addEventListener('click', function(){
-    if (cardName.value = false);
+//Function to validate the input details
+submit.addEventListener('onclick', validateDetails);
+
+function validateDetails(){
+    if (cardName = null);
         document.error.style.display = "grid";
         document.tag.href = "index.html";
-    if (cardNumber.value = false);
+    if (cardNumber = null);
         document.error.style.display = "grid";
         document.tag.href = "index.html";
-    if (cardDate.value = null);
+    if (cardDate = null);
         document.error.style.display = "grid";
         document.tag.href = "index.html";
-    if (cvcNumber.value = null);
+    if (cvcNumber = null);
         document.error.style.display = "grid";
         document.tag.href = "index.html";
-})
+}
 
 //Function to display details on the live card
-Input.addEventListener("active", Function(){
-    nameCard.innerHTML = cardName.value
+    // Input.addEventListener("click", function(){
+    //     nameCard.textContent = cardName.value
 
-})
+    // })
+    // label.addEventListener('mouseover', changeColor);
+
+    // function changeColor(){
+    //     label.style.color = "blue";
+    // }
