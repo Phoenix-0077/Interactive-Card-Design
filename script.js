08812804dbba7d7d1cb4d47a009e158da1bcdd85
Oluwatimilehin Dada <timilehinfavour14@gmail.com>
2023-02-06 20:22:27 +0100

    day6

--- script.js
+++ script.js
@@ -5,35 +5,22 @@ var cvcNumber = document.getElementById("cvcnumber");
 var submit = document.getElementsByTagName("button");
 var error = document.getElementsByClassName("error");
 var tag = document.getElementsByTagName("a");
-var Input = document.getElementsByTagName("input")
+var input = document.getElementsByTagName("input")
 var nameCard = document.getElementById("card-name")
 var label = document.getElementsByTagName("label")
 
 //Function to validate the input details
-submit.addEventListener('onclick', validateDetails);
+document.getElementsByTagName("button").addEventListener('click', function(){
+    if (input.length !== 0){
+        document.error.style.display = "none";
+    };
+});
 
-function validateDetails(){
-    if (cardName = null);
-        document.error.style.display = "grid";
-        document.tag.href = "index.html";
-    if (cardNumber = null);
-        document.error.style.display = "grid";
-        document.tag.href = "index.html";
-    if (cardDate = null);
-        document.error.style.display = "grid";
-        document.tag.href = "index.html";
-    if (cvcNumber = null);
-        document.error.style.display = "grid";
-        document.tag.href = "index.html";
-}
+// Function to display details on the live card
+    input.addEventListener("click", function(){
+        nameCard.textContent = cardName.value
+    })
 
-//Function to display details on the live card
-    // Input.addEventListener("click", function(){
-    //     nameCard.textContent = cardName.value
-
-    // })
-    // label.addEventListener('mouseover', changeColor);
-
-    // function changeColor(){
-    //     label.style.color = "blue";
-    // }
+    function changeColor(){
+        label.style.color = "blue";
+    }
