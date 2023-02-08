9d8b1d7a6b270e47a44726deca0cad217cba969e
Oluwatimilehin Dada <timilehinfavour14@gmail.com>
2023-02-08 20:35:38 +0100

    day8

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var cardName = document.getElementById("cardname");
+var cardName = document.querySelector("cardname");
 var cardNumber = document.getElementById("cardnumber");
 var cardDate = document.getElementsByClassName("date");
 var cvcNumber = document.getElementById("cvcnumber");
-var submit = document.getElementsByTagName("button");
+var submit = document.getElementById("button");
 var error = document.getElementsByClassName("error");
 var tag = document.getElementsByTagName("a");
 var input = document.getElementsByTagName("input")
@@ -10,11 +10,17 @@ var nameCard = document.getElementById("card-name")
 var label = document.getElementsByTagName("label")
 
 //Function to validate the input details
-
+console.log('This is me')
+submit.addEventListener('click', function(){
+    if(cardName.value().length() == 0 || cardNumber.value().length() == 0 ||
+    cardDate.value().length() == 0 || cvcNumber.length() == 0){
+        error.style.display = 'flex'
+    }
+});
 
 // Function to display details on the live card
-cardName.addEventListener('click', function(e){
-    console.log(cardName.length());
-    e.preventDefault();
-});
+// submit.addEventListener('click', function(e){
+//     alert('This is me');
+//     e.preventDefault();
+// });
 
