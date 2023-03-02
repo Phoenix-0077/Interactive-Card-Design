3570a272bb352ff0ae07159aab06664a05676e1b
Oluwatimilehin Dada <timilehinfavour14@gmail.com>
2023-03-02 01:49:03 +0100

    error message fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var cardName = document.querySelector("cardname");
+// var cardName = document.querySelector("cardname");
 var cardNumber = document.getElementById("cardnumber");
 var cardDate = document.getElementsByClassName("date");
 var cvcNumber = document.getElementById("cvcnumber");
@@ -8,18 +8,16 @@ var tag = document.getElementsByTagName("a");
 var input = document.getElementsByTagName("input")
 var nameCard = document.getElementById("card-name")
 var label = document.getElementsByTagName("label")
+var emptyName = document.forms["myForm"]["cardname"].value;
+var emptyNumber = document.forms["myForm"]["cardnumber"].value;
 
 //Function to validate the input details
 console.log('This is me')
 
-function validateForm(e){
-    let emptyName = document.forms["myForm"]["cardname"].value;
-    let emptyNumber = document.forms["myForm"]["cardnumber"].value;
-    if(emptyName || emptyNumber == ""){
+function validateForm(){
+    if(emptyName == ""){
         alert("Name and number must be filled");
         return false;
-    }else{
-        return true;
     }
 }
 
