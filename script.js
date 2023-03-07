8453e289d9a887d79c6d475881745a6f95203939
Oluwatimilehin Dada <timilehinfavour14@gmail.com>
2023-03-07 15:43:01 +0100

    js update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// var cardName = document.querySelector("cardname");
+var cardName = document.querySelector("cardname");
 var cardNumber = document.getElementById("cardnumber");
 var cardDate = document.getElementsByClassName("date");
 var cvcNumber = document.getElementById("cvcnumber");
@@ -8,18 +8,31 @@ var tag = document.getElementsByTagName("a");
 var input = document.getElementsByTagName("input")
 var nameCard = document.getElementById("card-name")
 var label = document.getElementsByTagName("label")
-var emptyName = document.forms["myForm"]["cardname"].value;
-var emptyNumber = document.forms["myForm"]["cardnumber"].value;
+
 
 //Function to validate the input details
 console.log('This is me')
 
+
 function validateForm(){
+    var emptyName = document.forms["myForm"]["cardname"].value;
     if(emptyName == ""){
         alert("Name and number must be filled");
         return false;
     }
+    var emptyNumber = document.forms["myForm"]["cardnumber"].value;
+    if(emptyNumber == ""){
+        alert("Name and number must be filled");
+        return false;
+    }
+    if(typeof emptyNumber == ""){
+        alert("Number entered is invalid");
+        return false;
+    }else{
+        return true; 
+    }
 }
+
 
 // Function to display details on the live card
 // submit.addEventListener('click', function(e){
