7e28ead7b23d71a2b64a3184c7db1440dbf24687
Oluwatimilehin Dada <timilehinfavour14@gmail.com>
2023-03-10 23:17:50 +0100

    code to validate card details

--- script.js
+++ script.js
@@ -1,69 +1,62 @@
-var cardName = document.querySelector("cardname");
-var cardNumber = document.getElementById("cardnumber");
-var cardDate = document.getElementsByClassName("date");
-var cvcNumber = document.getElementById("cvcnumber");
-var submit = document.getElementById("button");
-var error = document.getElementsByClassName("error");
-var tag = document.getElementsByTagName("a");
-var input = document.getElementsByTagName("input")
-var nameCard = document.getElementById("card-name")
-var label = document.getElementsByTagName("label")
-
-
 //Function to validate the input details
 console.log('This is me')
 
 
 function validateForm(){
-    //To validate input name
-    var emptyName = document.forms["myForm"]["cardname"].value;
-    // if(emptyName == ""){
-    //     alert("Name and number must be filled");
-    //     return false;
-    // }
-    //To validate input number
-    var emptyNumber = document.forms["myForm"]["cardnumber"].value;
-    // if(emptyNumber == ""){
-    //     alert("Name and number must be filled");
-    //     return false;
-    // }
-    // if(isNaN(emptyNumber) == true){
-    //     alert("Number entered is invalid");
-    //     return false;
-    // }
-    //To validate input date(month)
-    var dateMonth = document.forms["myForm"]["month"].value;
-    // if(dateMonth == ""){
-    //     alert("Number entered is invalid");
-    //     return false;
-    // }
-    // if(isNaN(dateMonth) == true){
-    //     alert("Number entered is invalid");
-    //     return false;
-    // }
-    // if(dateMonth > 12){
-    //     alert("Number entered is invalid");
-    //     return false;
-    // }
-    //To validate input date(year)
-    var dateYear = document.forms["myForm"]["year"].value;
-    // if(dateYear == ""){
-    //     alert("Year is empty");
-    //     return false;
-    // }
-    // if(isNaN(dateYear) == true){
-    //     alert("Year is not a number");
-    //     return false;
-    // }
-    // if(dateYear.length != 2){
-    //     alert("Year entered should be two digits");
-    //     return false;
-    // }
-    var cvcNumber = document.forms["myForm"]["cvcnumber"].value;
-    // if(isNaN(cvcNumber) == true && cvcNumber.length != 3){
-    //     alert("Number entered is invalid");
-    //     return false;
-    // }
+    emptyName = document.forms["myForm"]["cardname"].value;
+    if(emptyName == "") {
+        //To validate input number
+        var emptyNumber = document.forms["myForm"]["cardnumber"].value;
+        var cardNumberError = document.getElementById('error-message-card-number');
+        if(emptyNumber == ""){
+            cardNumberError.style.visibility = 'visible';
+        }
+        if(isNaN(emptyNumber) == true){
+            cardNumberError.style.visibility = 'visible';
+        }
+        if(emptyNumber.length != 16){
+            cardNumberError.style.visibility = 'visible';
+        }
+        // To validate input date(month)
+        var dateMonth = document.forms["myForm"]["month"].value;
+        var monthError = document.getElementById('error-message-date');
+        if(dateMonth == ""){
+            monthError.style.visibility = 'visible';
+        }
+        if(isNaN(dateMonth) == true){
+            monthError.style.visibility = 'visible';
+        }
+        if(dateMonth > 12){
+            monthError.style.visibility = 'visible';
+        }
+        //To validate input date(year)
+        var dateYear = document.forms["myForm"]["year"].value;
+        var yearError = document.getElementById('error-message-date');
+        if(dateYear == ""){
+            yearError.style.visibilty = 'visible';
+        }
+        if(isNaN(dateYear) == true){
+            yearError.style.visibility = 'visible';
+        }
+        if(dateYear.length != 2){
+            yearError.style.visibility = 'visible';
+        }
+        //To validate cvc number
+        var cvcNumber = document.forms["myForm"]["cvcnumber"].value;
+        var cvcError = document.getElementById('error-message-cvc');
+        if(cvcNumber == ""){
+            cvcError.style.visibility = "visible";
+        }
+        if(isNaN(cvcNumber) == true){
+            cvcError.style.visibility = "visible";
+        }
+        if(cvcNumber.length != 3){
+            cvcError.style.visibility = "visible";
+        }
+        return false;
+    }else{
+        return true;
+    }
 }
 
 
