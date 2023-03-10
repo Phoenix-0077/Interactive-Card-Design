1ecbde84f7574cd4479dfa55bba0c9697566bba4
Oluwatimilehin Dada <timilehinfavour14@gmail.com>
2023-03-10 22:24:12 +0100

    March 10

--- script.js
+++ script.js
@@ -15,36 +15,51 @@ console.log('This is me')
 
 
 function validateForm(){
-    // var emptyName = document.forms["myForm"]["cardname"].value;
+    //To validate input name
+    var emptyName = document.forms["myForm"]["cardname"].value;
     // if(emptyName == ""){
     //     alert("Name and number must be filled");
     //     return false;
     // }
-    // var emptyNumber = document.forms["myForm"]["cardnumber"].value;
+    //To validate input number
+    var emptyNumber = document.forms["myForm"]["cardnumber"].value;
     // if(emptyNumber == ""){
     //     alert("Name and number must be filled");
     //     return false;
     // }
-    // var emptyNumber = document.forms["myForm"]["cardnumber"].value;
     // if(isNaN(emptyNumber) == true){
     //     alert("Number entered is invalid");
     //     return false;
     // }
+    //To validate input date(month)
     var dateMonth = document.forms["myForm"]["month"].value;
-    if(isNaN(dateMonth) == true){
-        alert("Number entered is invalid");
-        return false;
-    }
-    if(dateMonth <= 1 && dateMonth >= 12){
-        alert("Number entered is invalid");
-        return false;
-    }
-    // var dateYear = document.forms["myForm"]["year"].value;
-    // if(isNaN(dateYear) == true ){
+    // if(dateMonth == ""){
     //     alert("Number entered is invalid");
     //     return false;
     // }
-    // var cvcNumber = document.forms["myForm"]["cvcnumber"].value;
+    // if(isNaN(dateMonth) == true){
+    //     alert("Number entered is invalid");
+    //     return false;
+    // }
+    // if(dateMonth > 12){
+    //     alert("Number entered is invalid");
+    //     return false;
+    // }
+    //To validate input date(year)
+    var dateYear = document.forms["myForm"]["year"].value;
+    // if(dateYear == ""){
+    //     alert("Year is empty");
+    //     return false;
+    // }
+    // if(isNaN(dateYear) == true){
+    //     alert("Year is not a number");
+    //     return false;
+    // }
+    // if(dateYear.length != 2){
+    //     alert("Year entered should be two digits");
+    //     return false;
+    // }
+    var cvcNumber = document.forms["myForm"]["cvcnumber"].value;
     // if(isNaN(cvcNumber) == true && cvcNumber.length != 3){
     //     alert("Number entered is invalid");
     //     return false;
